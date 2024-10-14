52de07c765eb711fea2e63da048ff4579aaa043b
RHrakib1 <rakibhasanmd457@gmail.com>
2024-10-15 00:10:12 +0600

    add to the update profile

--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
     const onSubmit = async (data) => {
         const { name, url } = data;
         try {
-            console.log(name,url);
+            console.log(name, url);
 
             newprofile(name, url)
         } catch (error) {
@@ -26,15 +26,23 @@ const Profile = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
+            <label className="label">
+                <span className="label-text">Name</span>
+            </label>
             {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register("name")} />
+            <input className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"  {...register("name")} />
 
             {/* include validation with required or other standard HTML validation rules */}
-            <input  {...register("url", { required: true })} />
+            <label className="label">
+                <span className="label-text">URL</span>
+            </label>
+            <input className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"  {...register("url", { required: true })} />
             {/* errors will return when field validation fails  */}
             {errors.exampleRequired && <span>This field is required</span>}
 
-            <input type="submit" />
+            <div className="form-control mt-6">
+                <button className="btn btn-primary">Update</button>
+            </div>
         </form>
     );
 };
